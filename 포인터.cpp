cc144228d8824f9c7a04b437688c20de9e54aba3
choiseonsic <152847811+choiseonsic@users.noreply.github.com>
2023-12-04 16:46:04 +0900

    포인터
    .

---
+++ 포인터.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int x = 70;
+	int *px = &x;
+	
+	printf("x의 값은 %d 입니다. \n", x);
+	printf("px의 값은 %d 입니다. \n", px);
+	printf("x의 주소값은 %d 입니다. \n", &x);
+	printf("px의 주소값은 %d 입니다. \n", *px);
+		
+} 
+
+
+
+
+
